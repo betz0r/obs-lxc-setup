07bbde24533c8cbb63568999644791420a8b6440
betz0r <s.betzinger@gmail.com>
2025-11-08 18:59:06 +0100

    latest gui-docker, Debian 13, DNS, MAC

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
-FROM bandi13/docker-obs
+FROM bandi13/gui-docker:latest
+
+RUN export DEBIAN_FRONTEND=noninteractive \
+    && apt-get update -y \
+    && apt-get install -y software-properties-common \
+    && add-apt-repository ppa:obsproject/obs-studio \
+    && apt-get update -y \
+    && apt-get install -y obs-studio \
+    && apt-get clean -y
+
+RUN echo "?package(bash):needs=\"X11\" section=\"DockerCustom\" title=\"OBS Screencast\" command=\"obs\"" >> /usr/share/menu/custom-docker && update-menus
 
 # Set environment variables for VNC password
 ENV VNC_PASSWD=123456
--- auto_deploy.sh
+++ auto_deploy.sh
@@ -4,21 +4,21 @@ set -e
 
 # User Configuration
 CONTAINER_NAME="obs-vnc-lxc"
-TEMPLATE="local:vztmpl/debian-11-standard_11.7-1_amd64.tar.zst"
+TEMPLATE="local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
 STORAGE="local-lvm"
 MEMORY=1024
 CORES=2
 SHARED_DIR="/host/shared"
 ARCH="amd64"
-DNS_SERVER="192.168.100.200"
+DNS_SERVER="192.168.1.162"
 
 # Function to check and download LXC template
 function check_template() {
-    echo "Checking for Debian 11 template..."
-    if ! pveam list local | grep -q "debian-11-standard"; then
-        echo "Template not found. Downloading the Debian 11 template..."
+    echo "Checking for Debian 13 template..."
+    if ! pveam list local | grep -q "debian-13-standard"; then
+        echo "Template not found. Downloading the Debian 13 template..."
         pveam update
-        if ! pveam download local debian-11-standard_11.7-1_amd64.tar.zst; then
+        if ! pveam download local debian-13-standard_13.1-2_amd64.tar.zst; then
             echo "Error: Failed to download template. Check your network or Proxmox storage."
             exit 1
         fi
@@ -39,7 +39,7 @@ function create_shared_dir() {
 # Function to prompt for Container ID
 function prompt_container_id() {
     while true; do
-        read -p "Enter the Container ID (e.g., 100): " CONTAINER_ID
+        read -p "Enter the Container ID (e.g., 120): " CONTAINER_ID
 
         # Check if input is a valid number
         if ! [[ "${CONTAINER_ID}" =~ ^[0-9]+$ ]]; then
@@ -96,7 +96,7 @@ pct create ${CONTAINER_ID} ${TEMPLATE} \
     --storage ${STORAGE} \
     --cores ${CORES} \
     --memory ${MEMORY} \
-    --net0 name=eth0,bridge=vmbr0,ip=dhcp \
+    --net0 name=eth0,bridge=vmbr0,ip=dhcp,hwaddr=BC:24:11:90:EA:23 \
     --password "${ROOT_PASSWORD}"
 
 # Step 6: Append configuration to LXC container
